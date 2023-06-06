c826d51b3a977530764233a9a1f20fa5dcfb4566
sagor@55 <45065471+sagor-ece17@users.noreply.github.com>
2023-06-06 21:38:48 +0600

    Add files via upload

---
+++ 5constructoranddestructor.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Rectangle {
+	int *width, *height;
+public:
+	Rectangle(int, int);
+	~Rectangle ();
+	int area () {return (*width * *height);}
+};
+
+Rectangle::Rectangle (int a, int b) { 
+	width= new int;
+	height = new int;
+	*width = a;
+	*height = b;
+}
+
+Rectangle:: ~Rectangle () { 
+	delete width;
+	delete height;
+}
+
+int main () {
+	Rectangle recta (3,4), rectb (5,6);
+	cout << "recta area: " << recta.area() << endl;
+	cout << "rectb area: " << rectb.area() << endl;
+	return 0;
+} 
