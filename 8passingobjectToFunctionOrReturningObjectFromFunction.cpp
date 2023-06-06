c2fb521b377213f5d37d3ad9149fa0f8e874b8a8
sagor@55 <45065471+sagor-ece17@users.noreply.github.com>
2023-06-06 22:36:57 +0600

    Add files via upload

---
+++ 8passingobjectToFunctionOrReturningObjectFromFunction.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+	int width, height;
+public:
+	Rectangle (int,int);
+	int area () {return (width*height);}
+};
+
+Rectangle::Rectangle (int a, int b) {
+	width = a; height = b;
+}
+
+Rectangle larger(Rectangle recta, Rectangle rectb){
+	if(recta.area()>rectb.area())
+		return recta;
+	else
+		return rectb;
+}
+
+int main () {
+	Rectangle recta (3,4);
+	Rectangle rectb (5,6);
+	Rectangle rect_larger(0,0);
+	
+	rect_larger=larger(recta, rectb);
+	
+	cout << "recta area: " << recta.area() << endl;
+	cout << "rectb area: " << rectb.area() << endl;
+	cout << "rect_larger area: " << rect_larger.area() << endl;
+	return 0;
+}
