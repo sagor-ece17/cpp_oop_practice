e0c85952405bb3ec96e53f8af1f56bd4f8f50159
sagor@55 <45065471+sagor-ece17@users.noreply.github.com>
2023-06-06 20:57:20 +0600

    first commit

---
+++ 1firstProgio.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char str[16];
+	cout << "Enter a string: ";
+	cin >> str;
+	cout << "You entered:"<< str;
+
+
+    return 0;
+
+}
---
+++ 2oopstart.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle{
+    int height, width;
+public:
+    void setvalues(int, int);
+    int area(){
+        return (height * width);
+    }
+};
+
+void Rectangle :: setvalues(int a, int b){
+    height = a;
+    width = b;
+}
+
+class Circle{
+    double radius;
+public:
+    void setRadius(double r){
+        radius = r;
+    }
+    double getDiameter(){
+        return radius*2;
+    }
+    double getArea();
+    double getCircumference();
+};
+double Circle :: getArea(){
+    return radius * radius * (22.0/7);
+}
+
+double Circle :: getCircumference(){
+    return 2 * radius * (22.0/7);
+}
+
+int main(){
+    Rectangle recta, rectb;
+    recta.setvalues(3, 4);
+    rectb.setvalues(5, 6);
+
+    cout << "recta area: "<< recta.area() << "\n";
+    cout << "rectb area " << rectb.area() << "\n";
+
+    Circle cir1;
+    cir1.setRadius(5);
+    cout << "Cir1 area: " << cir1.getArea() <<"\n";
+    cout << "Cir1 circumderence: " << cir1.getCircumference() <<"\n";
+    cout << "Cir1 Diameter: " << cir1.getDiameter() <<"\n";
+    return 0;
+}
